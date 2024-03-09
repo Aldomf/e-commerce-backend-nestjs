4f0ca91469cf70f41bf8ab275d36963d668bf446
Aldo Miralles <alditomiralles3@gmail.com>
2024-03-08 18:53:02 -0600

    adding guards to reviews

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -8,16 +8,22 @@ import {
   Delete,
   Patch,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
 import { Review } from './entities/review.entity';
+import { AuthGuard } from 'src/auth-module/guard/auth.guard';
+import { UserIdGuard } from 'src/common/guards/userId.guard';
+import { AdminGuard } from 'src/common/guards/admin.guard';
 
 @Controller('reviews')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
+  @UseGuards(UserIdGuard)
+  @UseGuards(AuthGuard)
   @Post(':userId/:productId')
   create(
     @Param('userId') userId: number,
@@ -27,6 +33,8 @@ export class ReviewController {
     return this.reviewService.create(userId, productId, createReviewDto);
   }
 
+  @UseGuards(UserIdGuard)
+  @UseGuards(AuthGuard)
   @Patch('/:userId/:id')
   async updateUserComment(
     @Param('userId', ParseIntPipe) userId: number,
@@ -36,6 +44,8 @@ export class ReviewController {
     return this.reviewService.updateUserComment(userId, id, updateReviewDto);
   }
 
+  @UseGuards(UserIdGuard)
+  @UseGuards(AuthGuard)
   @Delete('/:userId/:id')
   async deleteUserComment(
     @Param('userId', ParseIntPipe) userId: number,
@@ -51,16 +61,19 @@ export class ReviewController {
     return this.reviewService.findAllUserComments(productId);
   }
 
+  @UseGuards(AdminGuard)
   @Get()
   findAll(): Promise<Review[]> {
     return this.reviewService.findAll();
   }
 
+  @UseGuards(AdminGuard)
   @Get(':id')
   findOne(@Param('id') id: number): Promise<Review> {
     return this.reviewService.findOne(id);
   }
 
+  @UseGuards(AdminGuard)
   @Put(':id')
   update(
     @Param('id') id: number,
@@ -69,6 +82,7 @@ export class ReviewController {
     return this.reviewService.update(id, updateReviewDto);
   }
 
+  @UseGuards(AdminGuard)
   @Delete(':id')
   remove(@Param('id') id: number): Promise<void> {
     return this.reviewService.remove(id);
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -5,9 +5,15 @@ import { Review } from './entities/review.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from 'src/user-module/user.module';
 import { ProductModule } from 'src/product-module/product.module';
+import { AuthModule } from 'src/auth-module/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review]), UserModule, ProductModule],
+  imports: [
+    TypeOrmModule.forFeature([Review]),
+    UserModule,
+    ProductModule,
+    AuthModule,
+  ],
   controllers: [ReviewController],
   providers: [ReviewService],
 })
